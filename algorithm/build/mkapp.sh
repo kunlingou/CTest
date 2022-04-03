b331f55a2bd7d3719f92d963317d6eff84309c3c
kunlingou <kunlingou@foxmail.com>
2022-04-03 23:47:32 +0800

    测试框架

--- algorithm/build/mkapp.sh
+++ algorithm/build/mkapp.sh
@@ -1,6 +1,13 @@
+rm -rf ../output/*
 rm -rf __cache__
 mkdir __cache__
 cd __cache__
-cmake ../../src
+cmake ../..
 make
+
+cd ..
 rm -rf __cache__
+
+echo "==================run llt start====================="
+../output/test
+echo "==================run llt end====================="
